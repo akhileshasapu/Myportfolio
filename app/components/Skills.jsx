3a144bc2a0bbc59ba84ddff958806876e18dcfaf
akhileshasapu <156326254+akhileshasapu@users.noreply.github.com>
2025-07-04 00:25:44 +0530

    Update Skills.jsx

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -3,37 +3,40 @@
 import { motion } from "framer-motion";
 
 export default function Skills() {
-  const skills = [
-    
-    { name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
-    { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" },
-    { name: "C++", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
+ const skills = [
+  { name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
+  { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" },
+  { name: "C++", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
 
-  
-    { name: "HTML", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-    { name: "CSS", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-    { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-    { name: "Node.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
-    { name: "Express.js", logo: "https://www.peanutsquare.com/wp-content/uploads/2024/04/Express.png" },
-    { name: "React.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-    { name: "Next.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-    { name: "Tailwind CSS", logo: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/31/tailwind.png" },
-    { name: "Bootstrap", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
+  { name: "HTML", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+  { name: "CSS", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+  { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "Node.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+  { name: "Express.js", logo: "https://www.peanutsquare.com/wp-content/uploads/2024/04/Express.png" },
+  { name: "React.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+  { name: "Next.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
+  { name: "Tailwind CSS", logo: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/31/tailwind.png" },
+  { name: "Bootstrap", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
 
-   
-    { name: "MongoDB", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
-    { name: "SQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
+  { name: "MongoDB", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
+  { name: "SQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
 
-    
-    { name: "Git", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
+  { name: "Git", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
 
-    
-    { name: "Crypto Module", logo: "https://icmconference.org/wp-content/uploads/Icons-04-Post-Quantum-Crypto.png" },
-    { name: "Data Structures & Algorithms", logo: "https://cdn.hackr.io/uploads/topics/1527244435zqmNp2BGzh.png" },
-    { name: "OOPs", logo: "https://content-media-cdn.codefinity.com/articles/17c18c54-6ea9-443f-8255-a7f230a415ab/computer.png" },
-    { name: "SDLC", logo: "https://uniquesoftwaredev.com/wp-content/uploads/2023/08/Aug-B2-1.png" },
-    { name: "DevOps (Learning Stage)", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" },
-  ];
+  { name: "Crypto Module", logo: "https://icmconference.org/wp-content/uploads/Icons-04-Post-Quantum-Crypto.png" },
+  { name: "Data Structures & Algorithms", logo: "https://cdn.hackr.io/uploads/topics/1527244435zqmNp2BGzh.png" },
+  { name: "OOPs", logo: "https://content-media-cdn.codefinity.com/articles/17c18c54-6ea9-443f-8255-a7f230a415ab/computer.png" },
+  { name: "SDLC", logo: "https://uniquesoftwaredev.com/wp-content/uploads/2023/08/Aug-B2-1.png" },
+  { name: "DevOps (Learning Stage)", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" },
+
+  // 🔽 New Java Full Stack–related skills
+  { name: "Spring Boot", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" },
+  { name: "Spring Security", logo: "https://cdn.icon-icons.com/icons2/3914/PNG/512/springsecurity_logo_icon_248053.png" },
+  { name: "Hibernate", logo: "https://cdn.worldvectorlogo.com/logos/hibernate.svg" },
+  { name: "JSP", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
+  { name: "Servlets", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" }
+];
+
 
   return (
     <section
